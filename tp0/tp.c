a9016fdfc3c3023ee3c3c5f2fcafc28faf68bd68
Gaston.Martinez.90 <Gaston.Martinez.90@gmail.com>
2014-09-14 02:38:37 +0000

    Ahi se genera el archivo. El problema es que el fractal esta saliendo rotado

--- tp0/tp.c
+++ tp0/tp.c
@@ -4,29 +4,28 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define MAX_ITERATIONS 1000
-#define WIDTH 4
-#define HEIGHT 4
-#define RESOLUTION_X 640
-#define RESOLUTION_Y 480
-#define CENTER_REAL 0
-#define CENTER_IMAGINARY 0
+#define MAX_ITERATIONS 255
+#define WIDTH 0.005
+#define HEIGHT 0.005
+#define RESOLUTION_X 500
+#define RESOLUTION_Y 500
+#define CENTER_REAL 0.2820
+#define CENTER_IMAGINARY -0.0100
 
-float width = WIDTH;
-float height = HEIGHT;
-float resolutionX = RESOLUTION_X;
-float resolutionY = RESOLUTION_Y;
-float centerReal = CENTER_REAL;
-float centerImaginary = CENTER_IMAGINARY;
-
+static double width = WIDTH;
+static double height = HEIGHT;
+static int resolutionX = RESOLUTION_X;
+static int resolutionY = RESOLUTION_Y;
+static double centerReal = CENTER_REAL;
+static double centerImaginary = CENTER_IMAGINARY;
 
 /**
  * Devuelve la parte real de un numero complejo.
  * El parámetro es la parte real.
  */
-float getComplexPixelX(int pixelX) {
-	float complexResolution = width / resolutionX;
-	float value = (centerReal - (width / 2)) + complexResolution * pixelX;
+double getComplexPixelX(int pixelX) {
+	double complexResolution = width / resolutionX;
+	double value = (centerReal - (width / 2)) + complexResolution * pixelX;
 	//printf("CENTER REAL: %f - WIDTH: %f - COMPLEX_X: %f\n", centerReal, width, value);
 	return value;
 }
@@ -35,9 +34,9 @@ float getComplexPixelX(int pixelX) {
  * Devuelve la parte imaginaria de un numero complejo.
  * El parámetro es la parte imaginaria.
  */
-float getComplexPixelY(int pixelY) {
-	float complexResolution = height / resolutionY;
-	float value = (centerImaginary - (height / 2)) + complexResolution * pixelY;
+double getComplexPixelY(int pixelY) {
+	double complexResolution = height / resolutionY;
+	double value = (centerImaginary - (height / 2)) + complexResolution * pixelY;
 	//printf("CENTER IMAGINARY: %f - HEIGHT: %f - COMPLEX_Y: %f\n", centerImaginary, height, value);
 	return value;
 }
@@ -46,23 +45,23 @@ float getComplexPixelY(int pixelY) {
  * Devuelve el módulo de un numero complejo.
  * El primer parámetro es la parte real y el segundo es la imaginaria.
  */
-float getAbsolute(float pixelX, float pixelY) {
-	float real = pixelX * pixelX;
-	float imaginary = pixelY * pixelY;
+double getAbsolute(double pixelX, double pixelY) {
+	double real = pixelX * pixelX;
+	double imaginary = pixelY * pixelY;
 	return sqrt(real + imaginary);
 }
 
 /**
  * Trae la parte real de la nueva iteración. 
  */
-float getNewIterationX(float pixelX, float pixelY, float initialPixelX) {
+double getNewIterationX(double pixelX, double pixelY, double initialPixelX) {
 	return (pixelX * pixelX) - (pixelY * pixelY) + initialPixelX;
 }
 
 /**
  * Trae la parte imaginaria de la nueva iteración. 
  */
-float getNewIterationY(float pixelX, float pixelY, float initialPixelY) {
+double getNewIterationY(double pixelX, double pixelY, double initialPixelY) {
 	return (pixelX * pixelY) + (pixelX * pixelY) + initialPixelY;
 }
 
@@ -71,13 +70,13 @@ float getNewIterationY(float pixelX, float pixelY, float initialPixelY) {
  */
 int getBrightness(int pixelX, int pixelY) {
 	//printf("PIXEL X: %i - PIXEL Y: %i\n", pixelX, pixelY);
-	float initialComplexPixelX = getComplexPixelX(pixelX);
-	float initialComplexPixelY = getComplexPixelY(pixelY);
-	float varComplexPixelX = initialComplexPixelX;
-	float varComplexPixelY = initialComplexPixelY;
-	float previousComplexPixelX, previousComplexPixelY;
+	double initialComplexPixelX = getComplexPixelX(pixelX);
+	double initialComplexPixelY = getComplexPixelY(pixelY);
+	double varComplexPixelX = initialComplexPixelX;
+	double varComplexPixelY = initialComplexPixelY;
+	double previousComplexPixelX, previousComplexPixelY;
 	int brightness;
-	float limitation = 2;
+	double limitation = 2;
 	for (brightness = 0; brightness < MAX_ITERATIONS - 1; brightness++) {
 		if (getAbsolute(varComplexPixelX, varComplexPixelY) > limitation) {
 			break;
@@ -96,58 +95,67 @@ int getBrightness(int pixelX, int pixelY) {
 int main(int argc, char **argv) {
 	int c;
 
- 	char a = 'a';
+	char a = 'a';
 	char** end_ptr = NULL;
-
 
 	while (true) {
 		static struct option long_options[] = {
-		/* These options set a flag. */
-			{ "resolution", required_argument, 0, 'r' }, { "center",
-			required_argument, 0, 'c' }, { "width", required_argument, 0,
-			'w' }, { "height", required_argument, 0, 'H' }, { "output",
-			required_argument, 0, 'o' }, { "help", no_argument, 0, 'h' }, {
-			0, 0, 0, 0 } };
-	 	int option_index = 0;
+				/* These options set a flag. */
+				{ "resolution", required_argument, 0, 'r' },
+				{ "center",	required_argument, 0, 'c' },
+				{ "width", required_argument, 0,'w' },
+				{ "height", required_argument, 0, 'H' },
+				{ "output",	required_argument, 0, 'o' },
+				{ "help", no_argument, 0, 'h' },
+				{ 0, 0, 0, 0 }
+		};
+		int option_index = 0;
 		c = getopt_long(argc, argv, "rcwHo:h:", long_options, &option_index);
 		/* Detect the end of the options. */
 		if (c == -1)
 			break;
 
 		switch (c) {
-			case 'r':
-				puts("option -r\n");
-				break;
-			case 'c':
-				puts("option -c\n");
-				break;
-			case 'w':
-				strtol(optarg, end_ptr, 10);
-				if (end_ptr) {
-					printf("El parametro de w no es un numero");
-		 			return 2;
-		 		}
-		 		printf("option -w with value `%s'\n", optarg);
-		 		break;
-			case 'H':
-				printf("option -H with value `%s'\n", optarg);
-		 		break;
-			case 'f':
-				printf("option -f with value `%s'\n", optarg);
-		 		break;
-			case 'h':
-				printf("option -h\n");
-				break;
-		 	case '?':
-		 		break;
-			default:
-		 		break;
-		}
-	}
-	for (int pixelX = 0; pixelX <= resolutionX; pixelX++) {
-		for (int pixelY = 0; pixelY <= resolutionY; pixelY++) {
-			printf("PIXEL X: %i, PIXEL Y: %i, BRILLO: %i\n", pixelX, pixelY, getBrightness(pixelX, pixelY));
+		case 'r':
+			puts("option -r\n");
+			break;
+		case 'c':
+			puts("option -c\n");
+			break;
+		case 'w':
+			strtol(optarg, end_ptr, 10);
+			if (end_ptr) {
+				printf("El parametro de w no es un numero");
+				return 2;
+			}
+			printf("option -w with value `%s'\n", optarg);
+			break;
+		case 'H':
+			printf("option -H with value `%s'\n", optarg);
+			break;
+		case 'f':
+			printf("option -f with value `%s'\n", optarg);
+			break;
+		case 'h':
+			printf("option -h\n");
+			break;
+		case '?':
+			break;
+		default:
+			break;
 		}
 	}
 
+	FILE* output = fopen("a.pgm","w");
+	fprintf(output,"P2\n%d %d\n%d\n",resolutionX,resolutionY,MAX_ITERATIONS);
+
+	for (int pixelX = 0; pixelX <= resolutionX; pixelX++) {
+		for (int pixelY = 0; pixelY <= resolutionY; pixelY++) {
+			//printf("PIXEL X: %i, PIXEL Y: %i, BRILLO: %i\n", pixelX, pixelY,getBrightness(pixelX, pixelY));
+			fprintf(output,"%d ",getBrightness(pixelX, pixelY));
+		}
+		fprintf(output,"\n");
+	}
+
+	fclose(output);
 }
