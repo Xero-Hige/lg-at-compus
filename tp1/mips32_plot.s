2844feccfb8e682c5860870f481b254a8f9a239e
Gaston.Martinez.90 <Gaston.Martinez.90@gmail.com>
2014-10-12 19:26:45 +0000

    Agrego el base del archivo de assembler

---
+++ tp1/mips32_plot.s
@@ -0,0 +1,21 @@
+#include <mips/regdef.h>
+#include <sys/syscall.h>
+
+.text
+.align 2
+.globl mips32_plot
+.ent mips32_plot
+
+# void mips32_plot(param_t *parms)
+mips32_plot:
+	#ABI
+
+
+end:
+	#ABI
+	jr ra
+
+
+.end mips32_plot
+
+.data
